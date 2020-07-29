d033e007ff9b83ec09db4c0cb0b94a2a19632c95
superlooper615 <47366521+superlooper615@users.noreply.github.com>
2020-07-28 23:56:41 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,13 @@
+import random
+s=open("data/questions.txt","r")
+m=s.readlines()
+l=[]
+for i in range(0,len(m)-1):
+    x=m[i]
+    z=len(x)
+    a=x[:z-1]
+    l.append(a)
+l.append(m[i+1])
+o=random.choice(l)
+print(o)
+s.close()
